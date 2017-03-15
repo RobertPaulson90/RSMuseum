568d2bf98db53437ab30f6abbf787c6e80a870e1
Amivit <paul@wittig.dk>
2017-03-15 22:24:28 +0100

    Fixing _Layout.cshtml to define language is Danish, to prevent browser suggesting to translate

--- RSMuseum.ClassLibrary.Tests/Examples/TestTests.cs
+++ RSMuseum.ClassLibrary.Tests/Examples/TestTests.cs
@@ -17,11 +17,11 @@ namespace RSMuseum.ClassLibrary.Tests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
+        //[Fact]
+        //public void FailingTest()
+        //{
+        //    Assert.Equal(5, Add(2, 2));
+        //}
 
         private int Add(int x, int y)
         {
