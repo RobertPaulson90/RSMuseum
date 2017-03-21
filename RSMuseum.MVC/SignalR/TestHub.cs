173b3fa085774519a58862e629a98f3db65d2277
Amivit <paul@wittig.dk>
2017-03-21 21:43:20 +0100

    Added SignalR to project with chat example (promise to remember to remove!)

---
+++ RSMuseum.MVC/SignalR/TestHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace RSMuseum.MVC.SignalR
+{
+    public class TestHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
--- RSMuseum.MVC/Startup.cs
+++ RSMuseum.MVC/Startup.cs
@@ -13,6 +13,7 @@ namespace RSMuseum.MVC
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
