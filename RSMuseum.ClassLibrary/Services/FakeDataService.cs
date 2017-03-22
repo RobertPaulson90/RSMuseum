d9b80bd88b217450a278b1b9a9f04f24937a6425
Amivit <paul@wittig.dk>
2017-03-23 00:18:49 +0100

    Small change of var names

--- RSMuseum.ClassLibrary/Services/FakeDataService.cs
+++ RSMuseum.ClassLibrary/Services/FakeDataService.cs
@@ -32,23 +32,23 @@ namespace RSMuseum.ClassLibrary.Tests.Services
             {
                 var zipcodes = _dbCtx.ZipCodeTable.ToList();
                 var fakeAddress = new Faker<Address>()
-                    .RuleFor(o => o.ZipCode, f => f.PickRandom(zipcodes))
-                    .RuleFor(o => o.Street, f => f.Address.StreetAddress())
+                    .RuleFor(x => x.ZipCode, y => y.PickRandom(zipcodes))
+                    .RuleFor(x => x.Street, y => y.Address.StreetAddress())
                     .Generate();
 
                 var fakePerson = new Faker<Entities.Person>()
-                    .RuleFor(o => o.Address, f => fakeAddress)
-                    .RuleFor(o => o.Email, f => f.Internet.Email())
-                    .RuleFor(o => o.FirstName, f => f.Name.FirstName())
-                    .RuleFor(o => o.LastName, f => f.Name.LastName())
-                    .RuleFor(o => o.Phone, f => f.Random.Int(10000000, 99999999).ToString())
+                    .RuleFor(x => x.Address, y => fakeAddress)
+                    .RuleFor(x => x.Email, y => y.Internet.Email())
+                    .RuleFor(x => x.FirstName, y => y.Name.FirstName())
+                    .RuleFor(x => x.LastName, y => y.Name.LastName())
+                    .RuleFor(x => x.Phone, y => y.Random.Int(10000000, 99999999).ToString())
                     .Generate();
 
                 var fakeVolunteer = new Faker<Volunteer>()
-                    .RuleFor(o => o.Guilds, f => f.PickRandom(guilds, 2).ToList())
-                    .RuleFor(o => o.IsActive, f => f.Random.Bool())
-                    .RuleFor(o => o.MembershipNumber, f => f.Random.Int(0000, 9999))
-                    .RuleFor(o => o.Person, f => fakePerson)
+                    .RuleFor(x => x.Guilds, y => y.PickRandom(guilds, 2).ToList())
+                    .RuleFor(x => x.IsActive, y => y.Random.Bool())
+                    .RuleFor(x => x.MembershipNumber, y => y.Random.Int(0000, 9999))
+                    .RuleFor(x => x.Person, y => fakePerson)
                     .Generate();
 
                 _dbCtx.Volunteer.Add(fakeVolunteer);
