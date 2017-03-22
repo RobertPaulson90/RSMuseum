8c4060f23361a56b479aed9e0fc8f56171c6f4fd
Kasper Johnsen <kasperjohnsen@live.dk>
2017-03-22 11:22:10 +0100

    Cleaned up the DI container

--- RSMuseum.ClassLibrary/DI.cs
+++ RSMuseum.ClassLibrary/DI.cs
@@ -12,26 +12,14 @@ namespace RSMuseum.ClassLibrary
         // Access the container in other classes with DI.Container. Remember: using RSMuseum.ClassLibrary;
         public static Container Container { get; set; }
 
-        public DI(bool testing = false)
+        public DI()
         {
             Container = new Container();
             Container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
 
-            if (!testing)
-            {
-                Container.Register<RSMContext>(Lifestyle.Scoped);
-                Container.Register<IDbRepository, EntityFrameworkRepository>();
-                Container.Register<VolunteerService>();
-            }
-            else if (testing)
-            {
-                //var mockContext = new Mock<RSMContext>();
-                //Container.Register<RSMContext>(Lifestyle.Singleton);
-
-                //DI.Container.Register<ITestModel, TestModel>();
-                //DI.Container.Register<IDummyModel, DummyModel>();
-                // Register mocked interfaces instead
-            }
+            Container.Register<RSMContext>(Lifestyle.Scoped);
+            Container.Register<IDbRepository, EntityFrameworkRepository>();
+            Container.Register<VolunteerService>();
 
             Container.Verify();
         }
