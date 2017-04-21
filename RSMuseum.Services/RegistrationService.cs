79f57e4e977c84668054aeca4e128e3fe46c11fe
Kasper Johnsen <kasperjohnsen@live.dk>
2017-04-21 10:21:22 +0200

    Still working on Registration

--- RSMuseum.Services/RegistrationService.cs
+++ RSMuseum.Services/RegistrationService.cs
@@ -22,26 +22,13 @@ namespace RSMuseum.Services
 
         public bool AddRegistration(Registration registration)
         {
-
-            //JsonConvert.DeserializeObject<Registration>(registration);
-
-            var Registering = (Registration)registration;
-
-            //var guildsDTO = new List<IGuildDTO>(); //Instancisere en liste med de volunteer properties som vores View har brug for.
-
-            //var allGuilds = _dbRepo.GetAllGuilds(); //Går ned i vores DAL for at hente vores frivillige
-
-            //foreach (var item in allGuilds) //Smider data i vores VolunteerListe.
-            //{
-            //    var guildDTO = new GuildDTO()
-            //    {
-            //        GuildName = item.GuildName,
-            //        GuildId = item.GuildId
-            //    };
-            //    guildsDTO.Add(guildDTO);
-            //}
-
-            return true;
+            
+                _dbRepo.AddTimeRegistration(registration);
+                return true;
+            
+            
+               // return false;
+            
         }
     }
 }
