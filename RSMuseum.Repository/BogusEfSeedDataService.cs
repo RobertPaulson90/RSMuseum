082090ad013ab8335be79b3900ded4d08a62399a
Amivit <git@wittig.dk>
2017-06-06 00:18:36 +0200

    BogusEfSeedDataService modifications: - Minor performance increase by doing 2 db retrievals asynchronously - Removed an unnecessary if statement

--- RSMuseum.Repository/BogusEfSeedDataService.cs
+++ RSMuseum.Repository/BogusEfSeedDataService.cs
@@ -16,20 +16,20 @@ namespace RSMuseum.Repository
         // Consult Paul W. for any questions
 
         private readonly RSMContext _dbCtx;
-        
+
         public BogusEfSeedDataService(RSMContext dbCtx) {
-            _dbCtx = dbCtx;            
+            _dbCtx = dbCtx;
         }
 
         public async Task Initialize() {
-            await AddBogusGuilds(5);
-            await AddBogusZipcodes(25);
-            await AddBogusVolunteers(150);
-            await AddBogusRegistrationsRecentUnapproved(30);
-            await AddBogusRegistrationsLongTermApproved(600);
+            await MaintainBogusGuilds(5);
+            await MaintainBogusZipcodes(25);
+            await MaintainBogusVolunteers(150);
+            await MaintainBogusRegistrationsRecentUnapproved(30);
+            await MaintainBogusRegistrationsLongTermApproved(600);
         }
 
-        public async Task AddBogusGuilds(int count) {
+        private async Task MaintainBogusGuilds(int count) {
             var current = await _dbCtx.Guild.CountAsync();
             if (current >= count) {
                 return;
@@ -37,13 +37,13 @@ namespace RSMuseum.Repository
             count = count - current;
 
             for (int i = 0; i < count; i++) {
-                _dbCtx.Guild.Add(new Guild{GuildName = $"Laug {i}"});
+                _dbCtx.Guild.Add(new Guild { GuildName = $"Laug {i}" });
             }
 
             await _dbCtx.SaveChangesAsync();
         }
 
-        public async Task AddBogusZipcodes(int count) {
+        private async Task MaintainBogusZipcodes(int count) {
             var current = await _dbCtx.ZipCodeTable.CountAsync();
             if (current >= count) {
                 return;
@@ -53,23 +53,19 @@ namespace RSMuseum.Repository
             for (int i = 0; i < count; i++) {
                 var bogusZip = new Faker<ZipCodeTable>()
                     .RuleFor(x => x.City, y => y.Address.City())
-                    .RuleFor(x => x.ZipCode, y => y.Random.Int(1000,9999));
+                    .RuleFor(x => x.ZipCode, y => y.Random.Int(1000, 9999));
                 _dbCtx.ZipCodeTable.Add(bogusZip);
             }
 
             await _dbCtx.SaveChangesAsync();
         }
 
-        public async Task AddBogusVolunteers(int count) {
+        private async Task MaintainBogusVolunteers(int count) {
             var current = await _dbCtx.Volunteer.CountAsync();
             if (current >= count) {
                 return;
             }
             count = count - current;
-
-            if (await _dbCtx.Volunteer.CountAsync() >= count) {
-                return;
-            }
 
             var guilds = await _dbCtx.Guild.ToListAsync();
 
@@ -89,7 +85,7 @@ namespace RSMuseum.Repository
                     .Generate();
 
                 var fakeVolunteer = new Faker<Volunteer>()
-                    .RuleFor(x => x.Guilds, y => y.PickRandom(guilds, y.Random.Int(1,3)).ToList())
+                    .RuleFor(x => x.Guilds, y => y.PickRandom(guilds, y.Random.Int(1, 3)).ToList())
                     .RuleFor(x => x.IsActive, y => y.Random.Bool())
                     .RuleFor(x => x.MembershipNumber, y => y.Random.Int(1000, 9999))
                     .RuleFor(x => x.Person, y => fakePerson)
@@ -101,7 +97,7 @@ namespace RSMuseum.Repository
             await _dbCtx.SaveChangesAsync();
         }
 
-        public async Task AddBogusRegistrationsRecentUnapproved(int count) {
+        private async Task MaintainBogusRegistrationsRecentUnapproved(int count) {
             var current = await _dbCtx.Registration.Where(x => !x.Approved).CountAsync();
             if (current >= count) {
                 return;
@@ -111,8 +107,7 @@ namespace RSMuseum.Repository
             var guilds = await _dbCtx.Guild.ToListAsync();
             var volunteers = await _dbCtx.Volunteer.ToListAsync();
 
-            for (int i = 0; i < count; i++)
-            {
+            for (int i = 0; i < count; i++) {
                 var fakeDate = new Faker().Date.Recent(30);
                 var fakeRegistration = new Faker<Registration>()
                     .RuleFor(x => x.Hours, y => y.Random.Int(1, 6))
@@ -130,25 +125,26 @@ namespace RSMuseum.Repository
             await _dbCtx.SaveChangesAsync();
         }
 
-        public async Task AddBogusRegistrationsLongTermApproved(int count)
-        {
+        private async Task MaintainBogusRegistrationsLongTermApproved(int count) {
             var current = await _dbCtx.Registration.Where(x => x.Approved).CountAsync();
             if (current >= count) {
                 return;
             }
             count = count - current;
 
-            var guilds = await _dbCtx.Guild.ToListAsync();
-            var volunteers = await _dbCtx.Volunteer.ToListAsync();
+            var guildsTask = _dbCtx.Guild.ToListAsync();
+            var volunteersTask = _dbCtx.Volunteer.ToListAsync();
+
+            await Task.WhenAll(guildsTask, volunteersTask);
 
             for (int i = 0; i < count; i++) {
-                var fakeDate = new Faker().Date.Between(DateTime.Now, DateTime.Today.AddYears(-1));
+                var fakeDate = new Faker().Date.Between(DateTime.Now, DateTime.Today.AddYears(-1)); // Random day between now and 1 year ago
                 var fakeRegistration = new Faker<Registration>()
                     .RuleFor(x => x.Hours, y => y.Random.Int(1, 6))
                     .RuleFor(x => x.Date, y => fakeDate)
                     .RuleFor(x => x.Approved, y => true)
-                    .RuleFor(x => x.GuildId, y => y.PickRandom(guilds).GuildId)
-                    .RuleFor(x => x.VolunteerId, y => y.PickRandom(volunteers).VolunteerId)
+                    .RuleFor(x => x.GuildId, y => y.PickRandom(guildsTask.Result).GuildId)
+                    .RuleFor(x => x.VolunteerId, y => y.PickRandom(volunteersTask.Result).VolunteerId)
                     .RuleFor(x => x.DateTimeRegistered, y => fakeDate)
                     .RuleFor(x => x.Processed, y => true)
                     .Generate();
