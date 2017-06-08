3746796d3660b82d2bffc57468e53b6dfeb600a0
Amivit <git@wittig.dk>
2017-06-08 14:10:54 +0200

    Fixed a migration issue

--- RSMuseum.Repository/Migrations/Configuration.cs
+++ RSMuseum.Repository/Migrations/Configuration.cs
@@ -13,6 +13,7 @@ namespace RSMuseum.Repository.Migrations
     {
         public Configuration() {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(RSMContext context) {
